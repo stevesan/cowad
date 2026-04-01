be72e658a087a73f52ada9d44b5b5cd8297188c9
Steven An <stevenan@gmail.com>
2026-03-31 23:59:42 -0400

    Create extendCInRoom.test.ts

---
+++ tests/unit/extendCInRoom.test.ts
@@ -0,0 +1,24 @@
+import { describe, it, expect } from 'vitest';
+import { maps, setSelected, setMultiSelected, multiSelectType } from '../../src/state/appState';
+import { drawClick, drawComplete } from '../../src/map/drawSession';
+import { findVertexAt, findSectorAt, findLinedefNear, expectMapIsValid } from './setup';
+import { deleteSelected, deleteMultiSelected, mergeVertices, mergeSectors, bridgeLinedefs, placeThing, splitLinedefAtPoint } from '../../src/map/mapActions';
+
+describe('recorded test case', () => {
+  it('should produce correct map state', async () => {
+    await drawClick(16, 512);
+    await drawClick(720, 512);
+    await drawClick(768, -224);
+    await drawClick(0, -192);
+    await drawComplete();
+    await drawClick(176, 368);
+    await drawClick(608, 224);
+    await drawClick(160, -144);
+    await drawComplete();
+    await drawClick(176, 368);
+    await drawClick(496, 416);
+    await drawClick(608, 224);
+    expect(maps.sectors.size).toBe(3);
+    expectMapIsValid();
+  });
+});
