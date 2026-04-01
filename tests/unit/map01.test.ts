5c0ab3ebbf018e2e5f10b519c1639b2cfdead470
Steven An <stevenan@gmail.com>
2026-04-01 14:10:37 -0400

    Create map01.test.ts

---
+++ tests/unit/map01.test.ts
@@ -0,0 +1,49 @@
+import { describe, it, expect } from 'vitest';
+import { maps, setSelected, setMultiSelected, multiSelectType } from '../../src/state/appState';
+import { drawClick, drawComplete } from '../../src/map/drawSession';
+import { findVertexAt, findSectorAt, findLinedefNear, expectMapIsValid, dumpMapJSON } from './setup';
+import { deleteSelected, deleteMultiSelected, mergeVertices, mergeSectors, bridgeLinedefs, placeThing, splitLinedefAtPoint } from '../../src/map/mapActions';
+
+describe('map01', () => {
+  it('should produce correct map state', async () => {
+    await drawClick(-160, 176);
+    await drawClick(224, 160);
+    await drawClick(208, -96);
+    await drawClick(-176, -64);
+    await drawClick(-160, 176);
+    splitLinedefAtPoint(findLinedefNear(32, 168)!, -112, 176);
+    expect(maps.sectors.size).toBe(1);
+    expect(maps.vertices.size).toBe(5);
+    expect(maps.linedefs.size).toBe(5);
+
+    splitLinedefAtPoint(findLinedefNear(56, 168)!, -64, 176);
+    expect(maps.sectors.size).toBe(1);
+    expect(maps.vertices.size).toBe(6);
+    expect(maps.linedefs.size).toBe(6);
+
+    splitLinedefAtPoint(findLinedefNear(80, 168)!, 80, 160);
+    expect(maps.sectors.size).toBe(1);
+    expect(maps.vertices.size).toBe(7);
+    expect(maps.linedefs.size).toBe(7);
+
+    splitLinedefAtPoint(findLinedefNear(152, 160)!, 176, 160);
+    expect(maps.sectors.size).toBe(1);
+    expect(maps.vertices.size).toBe(8);
+    expect(maps.linedefs.size).toBe(8);
+
+    await drawClick(-112, 176);
+    await drawClick(32, -64);
+    await drawClick(176, 160);
+    await drawClick(-64, 176);
+    await drawClick(32, 16);
+    await drawClick(80, 160);
+    await drawClick(-64, 176);
+    await drawClick(-64, 320);
+    await drawClick(256, 336);
+    await drawClick(176, 208);
+    await drawClick(64, 240);
+    await drawClick(80, 160);
+    dumpMapJSON('map01');
+    expectMapIsValid();
+  });
+});
