fa5b1298ac9b0671c2c7871a3794cd77677a9817
Steven An <stevenan@gmail.com>
2026-04-04 23:27:32 -0400

    finish replacing sectorDrawing.test.ts

---
+++ tests/unit/bowtie.test.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from 'vitest';
+import { maps, setSelected, setMultiSelected, multiSelectType } from '../../src/state/appState';
+import { drawClick, drawComplete } from '../../src/map/drawSession';
+import { findVertexAt, findSectorAt, findLinedefNear, expectMapIsValid, dumpMapJSON } from './setup';
+import { deleteSelected, deleteMultiSelected, mergeVertices, mergeSectors, bridgeLinedefs, placeThing, splitLinedefAtPoint } from '../../src/map/mapActions';
+
+describe('bowtie', () => {
+  it('should produce correct map state', async () => {
+    await drawClick(-184, 296);
+    await drawClick(192, 296);
+    await drawClick(0, 32);
+    await drawComplete();
+    await drawClick(0, 32);
+    await drawClick(-192, -184);
+    await drawClick(160, -184);
+    await drawComplete();
+    dumpMapJSON('bowtie');
+    expectMapIsValid();
+  });
+});
---
+++ tests/unit/splitAndExtend.test.ts
@@ -0,0 +1,24 @@
+import { describe, it, expect } from 'vitest';
+import { maps, setSelected, setMultiSelected, multiSelectType } from '../../src/state/appState';
+import { drawClick, drawComplete } from '../../src/map/drawSession';
+import { findVertexAt, findSectorAt, findLinedefNear, expectMapIsValid, dumpMapJSON } from './setup';
+import { deleteSelected, deleteMultiSelected, mergeVertices, mergeSectors, bridgeLinedefs, placeThing, splitLinedefAtPoint } from '../../src/map/mapActions';
+
+describe('splitAndExtend', () => {
+  it('should produce correct map state', async () => {
+    await drawClick(-208, 240);
+    await drawClick(104, 240);
+    await drawClick(128, -72);
+    await drawClick(-152, -48);
+    await drawComplete();
+    await drawClick(-208, 240);
+    await drawClick(128, -72);
+    await drawClick(-208, 240);
+    await drawClick(-40, 336);
+    await drawClick(-208, 240);
+    await drawClick(272, 360);
+    await drawClick(128, -72);
+    dumpMapJSON('splitandextend');
+    expectMapIsValid();
+  });
+});
